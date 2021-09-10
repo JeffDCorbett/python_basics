2c3e2430eb6d9104ea7223ac010cc5c5eeeafeb9
Jeff Corbett <jcorbett@dejotech.com>
2021-09-10 13:40:47 -0400

    Added ch6_functions.py

---
+++ ch6_functions.py
@@ -0,0 +1,15 @@
+
+def cube(x):
+    answer = x ** 3
+    return answer
+
+
+cube_result = cube(3)
+print(f"The result of 3 cubed is {cube_result}")
+
+
+def greet(name):
+    print(f"Hello {name}")
+
+
+greet("Jeff")
